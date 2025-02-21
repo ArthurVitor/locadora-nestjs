70fddb770fe028844851acb9d7161d0576472f7d
ArthurVitor <arthurvitorgn@gmail.com>
2025-02-21 14:47:12 -0300

    feat; login test

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,8 +8,8 @@ import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './infra/security/guard/auth.guard';
 import { RolesGuard } from './infra/security/guard/roles.guard';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/user/entities/User.entity';
 import { UserRepository } from 'src/user/repositories/user.repository';
+import { User } from 'src/user/entities/User.entity';
 
 @Module({
   imports: [
